f2638b721c2105c24820787d7be0bcace65ff085
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2023-03-14 12:13:50 +0300

    eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,6 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')  
   .version('0.0.1', '-V, --version', 'output the version number')
-  //.parse(process.argv)
   .argument('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
-program.parse();
+  .parse();
