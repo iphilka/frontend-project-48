12e4fdcf5cf7b811fa77825480208d39d02dbcce
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2023-03-28 13:28:54 +0300

    f*cking bull shit

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,5 +8,34 @@ const stringify = (data, deth, mapping) => {
     }
 
     const output = Object.entries(data)
-        .map(([key, values]) => )
+        .map(([key, value]) => mapping.unchanged({key, value}, depth + 1))
 };
+
+const mapping = {
+    root: ({children}, depth, iter) => {
+        const output = children.flatMap((node) => mapping[node, type](node, depth + 1, iter));
+        return `{\n${output.join('\n')}\n}`
+    },
+    nested: ({ key, children }, depth, iter) => {
+        const output = children.flatMap((node) => mapping[node, type](node, depth + 1, iter));
+        return `${ident(depth)}  ${key}: {\n${output.join('\n')}\n${ident(depth)}}`
+    },
+    added: (node, depth) => `${ident(depth)}+ ${node.key}: ${stringify(node, depth, mapping)}`,
+    deleted: (node, depth) => `${ident(depth)}- ${node.key}: ${stringify(node, depth, mapping)}`,
+    unchanged: (node, depth) => `${ident(depth)}  ${node.key}: ${stringify(node, depth, mapping)}`,
+    changed: (node, depth) => {
+        const { key, value1, value2 } = node;
+        
+        const data1 = `${ident(depth)}- ${key}: ${stringify(value1, depth, mapping)}`;
+        const data2 = `${ident(depth)}+ ${key}: ${stringify(value2, depth, mapping)}`;
+
+        return [data1, data2];
+    },
+}
+
+const renderThree = (ast) => {
+    const iter = (node, depth) => mapping[node.type](node, depth, iter);
+    return iter(ast, 0);
+};
+
+export default renderThree;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
+import parse from './parse.js';
+import format from './formatters/index.js'
+import buildTree from './treeBuider.js'
 
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getData = (filepath) => parse(fs.readFileSync(filepath), 'utf-8');
+
+const extractFormat = (filepath) => parse.extname(filepath).slice(1);
+
+const genDiff = (path1, path2, formstName = 'stylish') => {
+    const data1 = getData(buildFullPath(path1));
+    const data2 = getData(buildFullPath(path2));
+
+    const tree = build(data1, data2);
+
+    return format(tree, formatName);
+
+}
 // const genDiff = (arrPath) => {
 //     const pathStatic = '../fixture/'
 //     const arrData = arrPath.map(item => JSON.parse(fs.readFileSync(pathStatic + item, 'utf-8')))
@@ -9,26 +26,26 @@ import _ from 'lodash';
 // };
 
 
-const genDiff = (path1, path2) => {
-    const data1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path1) , 'utf-8'));
-    const data2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path2) , 'utf-8'));
-    const arrPattern = []
-    const arrData = _.sortBy([].concat(Object.keys(data1), Object.keys(data2)));
-    arrData.forEach(item => {
-        if(item in data1 && item in data2 && data1[item] === data2[item]){
-            arrPattern.push(`  ${item} : ${data2[item]}`)
-        }else if(item in data1 && item in data2 && data1[item] !== data2[item]){
-            arrPattern.push(`- ${item} : ${data1[item]}`,`+ ${item} : ${data2[item]}`)
-        }else if(item in data1){
-            arrPattern.push(`- ${item} : ${data1[item]}`)
-        }else{
-            arrPattern.push(`+ ${item} : ${data2[item]}`)
-        }
-    })
+// const genDiff = (path1, path2) => {
+//     const data1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path1) , 'utf-8'));
+//     const data2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path2) , 'utf-8'));
+//     const arrPattern = []
+//     const arrData = _.sortBy([].concat(Object.keys(data1), Object.keys(data2)));
+//     arrData.forEach(item => {
+//         if(item in data1 && item in data2 && data1[item] === data2[item]){
+//             arrPattern.push(`  ${item} : ${data2[item]}`)
+//         }else if(item in data1 && item in data2 && data1[item] !== data2[item]){
+//             arrPattern.push(`- ${item} : ${data1[item]}`,`+ ${item} : ${data2[item]}`)
+//         }else if(item in data1){
+//             arrPattern.push(`- ${item} : ${data1[item]}`)
+//         }else{
+//             arrPattern.push(`+ ${item} : ${data2[item]}`)
+//         }
+//     })
 
-    const diff = Object.fromEntries(_.union((arrPattern.map(item => item.split(' : ')))))
-    console.log(diff)
-}
+//     const diff = Object.fromEntries(_.union((arrPattern.map(item => item.split(' : ')))))
+//     console.log(diff)
+// }
 
 // const test2 =  path.resolve(process.cwd(), 'file1.json');
 
