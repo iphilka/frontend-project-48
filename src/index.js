18ef5c78215cab1bc4e2b3494a0786dcd64a0c2e
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2023-04-04 11:17:45 +0300

    asciinema

--- src/index.js
+++ src/index.js
@@ -26,45 +26,5 @@ const genDiff = (path1, path2) => {
 };
 
 
-// const test2 =  path.resolve(process.cwd(), 'file1.json');
-
-
-// const keys1 = _.union(Object.keys(data1));
-// const keys2 = _.union(Object.keys(data2))
-// const arrData =  _.sortBy([].concat(keys1, keys2));
-// const keys = _.union(Object.keys(data1), Object.keys(data2));
-// const sortedKeys = _.sortBy(keys);
-// for (const key of sortedKeys) {
-//     if (data1.hasOwnProperty(key) && !data2.hasOwnProperty(key)) {
-//         const firstFile = `- ${key}: ${data1[key]}`
-//         console.log(firstFile);
-//         //return firstFile;
-//     }
-//     else if (data1.hasOwnProperty(key) && data2.hasOwnProperty(key) && data1[key] === data2[key]) {
-//         const twoFile =  `  ${key}: ${data1[key]}`;
-//         console.log(twoFile);
-//     } 
-//     else if (data1.hasOwnProperty(key) && data2.hasOwnProperty(key) && data1[key] !== data2[key]) {
-//         const another = `- ${key}: ${data1[key]}  
-//         + ${key}: ${data2[key]}`
-//         return another;
-//     }
-// }
-
-//     // const diff = sortedKeys.map((key) => {
-   
-//     // })
-//     console.log(diff);
-// };
-    // if(key in sortedKeys && data1[key] != data2[key]){
-    //     return `+ ${key} : ${data1[key]}`;
-    // }
-    // if (key in sortedKeys && data1[key] && !data2[key] ) {
-    //     return `- ${key} : ${data1[key]}`;
-    // } 
-
- 
-
-
 
 export default genDiff;
